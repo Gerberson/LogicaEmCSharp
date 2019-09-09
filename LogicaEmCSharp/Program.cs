cf718f117c174379ff6c70a1507b4442983a01f0
Gerberson Dias <gerbersondias.312@gmail.com>
2019-09-09 10:13:37 -0300

    Adicionar arquivos de projeto.

---
+++ LogicaEmCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicaEmCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
